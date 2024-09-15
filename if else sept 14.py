8d94ca25655b417b776d4e0b9e0c6c391b871e98
nissycode <158865246+nissycode@users.noreply.github.com>
2024-09-15 15:10:08 +0800

    Add files via upload

---
+++ if else sept 14.py
@@ -0,0 +1,33 @@
+num = int(input("Enter a number: "))
+
+# check if the number is positive or neegative
+
+'''if num > 0:
+    print("It is a positive number")
+elif num == 0:
+    print("It is a ZERO")
+else: 
+    print("It is a negative number")
+    
+sum = 0'''
+#summation or operation
+
+'''if num != 0:
+    sum += num # 0 + 9
+    #sum = sum + num
+    print(sum)'''
+
+# nested if
+
+if num >= 0:
+    
+    if num == 0: 
+        print("It is a ZERO")
+        if num >=  and num <= :
+            
+    else:
+        print("It is a positive number")
+    
+else:
+    print("It is negative")
+    
