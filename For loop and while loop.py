472cc485674852a6df81c54fb781d9716dd52f58
nissycode <158865246+nissycode@users.noreply.github.com>
2024-09-21 11:10:57 +0800

    Update For loop and while loop.py

--- For loop and while loop.py
+++ For loop and while loop.py
@@ -1,27 +1,51 @@
-def fareDiscount(dependent, fare):
-    discount = 0
 
-    if dependent == 0:
-        discount = 0
-    elif 1 <= dependent <= 2:
-        discount = 0.10
-    elif 3 <= dependent <= 4:
-        discount = 0.20
-    else:
-        discount = 0.30
+def fareDiscount(fare, dependents):
+
+    discount = 0
+    
+    while True:
+        if dependents == 0:
+            discount = 0
+            break
+        elif 1<= dependents <=2:
+            discount = 0.10
+            break
+        elif 3<= dependents <=4:
+            discount = 0.20
+            break
+        else:
+            discount = 0.30
+            break
             
     totalFare = fare - (fare * discount)
     return totalFare
-
-passenger = int(input("How many passengers: "))
-fare = float(input("Fare: "))
-
-for i in range(passenger):
-    print(f"\nPassenger {i+1}: ")
-   f
-    age = int(input("Passenger Age: "))
-    dependents = int(input("Number of dependents: "))
     
-    totalFare = fareDiscount(dependents, fare)
-    print(f"Total fare for passenger {i+1}: {totalFare :.2f}")
-
+    
+passengerCount = int(input("How many passenger:" ))
+fare = float(input("Full fare:" ))
+    
+for i in range(passengerCount):
+    print(f"\nPassenger {i + 1}:" )
+    
+    age = int(input("Enter your age:" ))
+    dependents = int(input("Number of Dependents:" ))
+    
+    while True:
+        if age < 18:
+            discount = 0.50
+            break
+        elif age >= 60:
+            discount = 0.20
+            break
+        else:
+            discount = 0
+            break
+    
+    totalDiscount = fare * discount
+    totalFare = fareDiscount(fare, dependents)
+    toPay = totalFare - totalDiscount
+    
+    print(f"Total Payment: {totalFare: .2f}")
+    print(f"Total Discount: {totalDiscount: .2f}")
+    print(f"To Pay: {toPay: .2f}")
+    
