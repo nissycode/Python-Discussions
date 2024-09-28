ed58903e243cc9ddd49cc74bb989de01d114a7da
nissycode <158865246+nissycode@users.noreply.github.com>
2024-09-28 09:52:02 +0800

    Update

---
+++ Activity about looping.py
@@ -0,0 +1,23 @@
+for i in range(1,11):
+    print(i)
+
+print("************************** ")   
+sum = 0
+for i in range(1,21):
+    if i % 2 == 0:
+        sum += 0
+        
+print(sum)  
+
+print("************************* ")  
+
+sum = 0
+num = 0
+
+while num <= 100:
+    num = int(input("Enter a number: "))
+    
+    if num <= 100:
+        sum += num
+        
+print(sum)        
