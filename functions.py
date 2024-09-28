6d9f5235fb271c9d5b0a1242b1aebec25c75a1af
nissycode <158865246+nissycode@users.noreply.github.com>
2024-09-28 10:59:01 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,26 @@
 #FUNCTIONS =  BLOCK OF CODE
-
+'''
 def variableName():
   BLOCK CODE
 
-variableName()
+variableName() '''
 
+def greet(section):
+  print("Welcome to python class, section)
+  
+greet("BSIT 2101)
+
+#arguments
+
+def total_sum(num1,num2):
+  add = num1+ num2
+  print(add)
+  
+total_sum(100,2)
+
+#***************************8
+def square(num)
+  result = num * num
+  return result
+
+square = 
