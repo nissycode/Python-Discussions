200c99b59224e10d12ad9bb0581e1eacc8a49cd2
nissycode <158865246+nissycode@users.noreply.github.com>
2024-09-28 11:18:58 +0800

    Update functions.py

--- functions.py
+++ functions.py
@@ -23,4 +23,64 @@ def square(num)
   result = num * num
   return result
 
-square = 
+square = square(10)
+
+print()
+sqrt()
+pow()
+
+
+#*************************************************
+
+import math
+
+sqrt = math.sqrt(4)
+print(sqrt)
+
+power = pow(10,5)
+print(power)
+ #**************************************************
+#compute area of shapes(triangle, rectangle, circle)
+import math
+
+def triangle(base, height):
+  return (base * height) / 2
+
+def rectangle(length, width):
+  return height * width
+
+def circle():
+
+def main():
+  base = 100
+  height = 20
+  triangle(base, height)
+  print("Area of Triangle is: " + triangle)
+
+  length = 100
+  width = 200
+  print("Area of Rectangle is: " + rectangle)
+  
+main()
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
